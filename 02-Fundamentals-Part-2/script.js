5328fac91ae569ff80299b88b56664c9b90a672c
s_zervos <s_zervos@wargaming.net>
2024-02-20 17:05:34 +0200

    Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true;
+}
+
+//undefined variable bug
+/*
+if (hasDriversLicense) {
+  console.log("I can drive :D");
+}
+*/
+
+//reserved word bug
+/*
+const private = 534;
+*/
