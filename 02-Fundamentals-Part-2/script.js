38585ca9e251c56fd4e1242450c1a33b5f3c2d82
s_zervos <s_zervos@wargaming.net>
2024-02-27 09:56:25 +0200

    Basic Array Operations & Introduction to Objects

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -58,3 +58,38 @@ console.log(years);
 
 friends[2] = "Neritshki"; //changes the value even tho array is const
 console.log(friends);
+
+/** BASIC ARRAY OPERATIONS */
+friends.push("Maschini"); //adds at the end
+const popped = friends.pop(); //pops the end
+console.log(popped); //pop() returns the popped element
+
+friends.unshift("Dimitra"); //adds at the beginning
+friends.shift(); //removes from beginning
+
+console.log(friends.indexOf("Neri"));
+console.log(friends.includes("Spiros")); //uses strict equality
+
+/** INTRODUCTION TO OBJECTS */
+/*
+const spirosArray = ["Spiros", "Zervos", 2037 - 1996, "programmer", friends];
+
+or
+
+const spirosArray = [
+  "Spiros",
+  "Zervos", 
+  2037 - 1996, 
+  "programmer", 
+  friends
+];
+*/
+//but if we wanted to reference these data by what they denote - eg job, name, surname, etc - we cannot. That's why we have the object!
+
+const spiros = {
+  firstName: "Spiros",
+  lastName: "Zervos",
+  age: 2037 - 1996,
+  job: "programmer",
+  friends: friends,
+};
