c82f2ee809b2dd857ccc12f75146b37e7cbcb2c4
s_zervos <s_zervos@wargaming.net>
2024-04-16 15:45:53 +0300

    Section 7: Coding Challenge #1

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -7,12 +7,27 @@ const inputElement = document.querySelector(".guess");
 const scoreElement = document.querySelector(".score");
 const numberElement = document.querySelector(".number");
 const btnCheck = document.querySelector(".check");
+const btnAgain = document.querySelector(".again");
 ///
 
 /** Number that needs to be guessed */
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 
+/** On btn Again! clicked */
+btnAgain.addEventListener("click", function () {
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+
+  bodyElement.style.backgroundColor = "#222";
+  numberElement.style.width = "15rem";
+  numberElement.textContent = "?";
+  messageElement.textContent = "Start guessing...";
+  scoreElement.textContent = score;
+  inputElement.value = "";
+});
+
+/** On btn check! clicked */
 btnCheck.addEventListener("click", function () {
   const guess = Number(inputElement.value);
 
