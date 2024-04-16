31e0d06e7cf9b11fb437993c0edd8a43a887878d
s_zervos <s_zervos@wargaming.net>
2024-04-16 16:30:43 +0300

    Working With Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,10 +1,25 @@
-'use strict';
+"use strict";
 
 /// DOM elements init
-const modalElement = document.querySelector('.modal');
-const overlayElement = document.querySelector('.oerlay');
-const btnCloseModal = document.querySelector('.close-modal');
-const btnsShowModal = document.querySelectorAll('.show-modal');
+const modalElement = document.querySelector(".modal");
+const overlayElement = document.querySelector(".oerlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsShowModal = document.querySelectorAll(".show-modal");
 ///
 
-for (let i = 0; i < btnsShowModal[i].clientHeight; i++) {}
+const openModal = function () {
+  modalElement.classList.remove("hidden");
+  overlayElement.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modalElement.classList.add("hidden");
+  overlayElement.classList.add("hidden");
+};
+
+for (let i = 0; i < btnsShowModal[i].clientHeight; i++) {
+  btnsShowModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlayElement.addEventListener("click", closeModal);
