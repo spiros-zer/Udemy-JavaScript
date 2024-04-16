8f53350c1e0074d510aa3016af0c7ff4a7044554
s_zervos <s_zervos@wargaming.net>
2024-04-16 16:57:12 +0300

    Rolling the Dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,31 @@
 "use strict";
 
-/** DOM elements init */
+/// DOM elements init
 const playerScoreLEl = document.querySelector("#score--0");
 const playerScoreREl = document.querySelector("#score--1");
+const currentScoreLEl = document.querySelector("#current--0");
+const currentScoreREl = document.querySelector("#current--1");
 const diceEl = document.querySelector(".dice");
+const btnRollEl = document.querySelector(".btn--roll");
+const btnHoldEl = document.querySelector(".btn--hold");
+///
+
+let currenScore = 0;
+let scorePlayerL = 0;
+let scorePlayerR = 0;
 
 playerScoreLEl.textContent = "0";
 playerScoreREl.textContent = "0";
-
 diceEl.classList.add("hidden");
+
+btnRollEl.addEventListener("click", function () {
+  const dice = Math.trunc(Math.random() * 6 + 1);
+
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove("hidden");
+
+  if (dice !== 1) {
+    currenScore += dice;
+  } else {
+  }
+});
