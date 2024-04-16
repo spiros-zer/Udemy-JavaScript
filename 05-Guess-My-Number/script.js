e53f1db8ad39a56546b151a91cf56ebbf580b466
s_zervos <s_zervos@wargaming.net>
2024-04-16 15:31:20 +0300

    Implementing the Game Logic

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,3 +1,39 @@
 "use strict";
 
-document.querySelector(".message");
+/// DOM element init
+const messageElement = document.querySelector(".message");
+const inputElement = document.querySelector(".number");
+const scoreElement = document.querySelector(".score");
+const numberElement = document.querySelector(".number");
+const btnCheck = document.querySelector(".check");
+///
+
+/** Number that needs to be guessed */
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+
+btnCheck.addEventListener("click", function () {
+  const guess = Number(inputElement.value);
+
+  if (!guess) {
+    messageElement.textContent = "🚫 No Number!";
+  } else if (guess === secretNumber) {
+    messageElement.textContent = "🎉 Correct Number";
+  } else if (guess > secretNumber) {
+    if (score) {
+      messageElement.textContent = "📈 Too High!";
+      score--;
+      scoreElement.textContent = score;
+    } else {
+      messageElement.textContent = "💥 You lost!";
+    }
+  } else {
+    if (score) {
+      messageElement.textContent = "📉 Too Low!";
+      score--;
+      scoreElement.textContent = score;
+    } else {
+      messageElement.textContent = "💥 You lost!";
+    }
+  }
+});
