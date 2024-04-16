8a6258bcbee0a391d5c0743fea4866406c72dcb5
s_zervos <s_zervos@wargaming.net>
2024-04-16 13:33:51 +0300

    Section 5: Coding challenge #1

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,12 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
+
+function printForecast(arr) {
+  let outputStr = "";
+  for (let i = 0; i < arr.length; i++) {
+    outputStr += `... ${arr[i]} in ${i + 1} day${i + 1 > 1 ? "s" : ""} `;
+  }
+  console.log(outputStr);
+}
+
+printForecast([20, 20, 22, 25, 20]);
