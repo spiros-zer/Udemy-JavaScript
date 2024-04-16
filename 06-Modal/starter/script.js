e0ace7c1fbe0138175ff0ad8fad322118ef41368
s_zervos <s_zervos@wargaming.net>
2024-04-16 16:20:28 +0300

    Section 7: PROJECT #2: Modal Window

---
+++ 06-Modal/starter/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/// DOM elements init
+const modalElement = document.querySelector('.modal');
+const overlayElement = document.querySelector('.oerlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+///
+
+for (let i = 0; i < btnsShowModal[i].clientHeight; i++) {}
